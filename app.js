d4753337bb93b46c889fda02c12f6e504bf7f543
RuizHenrique01 <affonsohenriqueruiz@gmail.com>
2021-09-27 10:31:52 -0400

    Adicionando novamente a mensagem de boas-vindas

--- app.js
+++ app.js
@@ -15,6 +15,14 @@ app.use('/uploads', express.static('uploads'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+app.get("/", (request, response) => {
+    return response.json({ 
+        "message" : "Welcome to Study Manager API",
+        "Link da documentação da API" : "https://github.com/RuizHenrique01/Study_Manager-API/blob/master/documentation.md",
+        "Link da API no GitHub" : "https://github.com/RuizHenrique01/Study_Manager-API"
+    });
+});
+
 app.use("/user", userRouter);
 app.use("/projects", projectRouter);
 app.use("/projects", taskController);
@@ -23,7 +31,7 @@ app.use((err, request, response, next) => {
     if (err instanceof Error)
         return response.status(400).json({ error: err.message });
 
-    return response.status(500).json({ error: "Internal Server Error!"});
+    return response.status(500).json({ error: "Internal Server Error!" });
 });
 
 module.exports = app;
